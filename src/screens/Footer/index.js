179ec9373aa6897a87b21f6e9cf0fbdba7e266de
Ahmad Hazim Ahmad Fuad <ahmad.hazim@devwiz.my>
2024-01-11 04:24:41 +0800

    update footer line

--- src/screens/Footer/index.js
+++ src/screens/Footer/index.js
@@ -4,8 +4,9 @@ function Footer() {
   return (
     <div className="absolute bottom-0 left-0 right-0 bg-slate-900">
       <p className="text-slate-500 text-xs py-1 text-center font-light">
-        Copyright Reserved MobileMind-Pro© 2024 . Visit www.mobilemind.pro for
-        more info
+        This project is currently in progress, and it is advisable to anticipate
+        potential errors. We appreciate your understanding and cooperation.
+        Thank you
       </p>
     </div>
   );
